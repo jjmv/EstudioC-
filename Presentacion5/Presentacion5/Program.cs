bcff887dbbd12c0911a32bd75d9696c34e2b54a4
Jjmv <jjmv.97@hotmail.com>
2016-12-29 23:48:13 -0600

    Learning POO

---
+++ Presentacion5/Presentacion5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion del objeto empleado
+            clEmpleados empleado;
+            //Aqui se instancia el objeto
+            empleado = new clEmpleados();
+
+            empleado.Edad = 25;
+            empleado.Nombre = "Javier";
+            empleado.SueldoDiario = 12.5m;
+            decimal total;
+            total = empleado.CalculaSalario(30);
+            System.Console.WriteLine($"El salario mensual de {empleado.Nombre}");
+            System.Console.WriteLine($"Es de {total}");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Presentacion5/Presentacion5/clEmpleados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion5
+{
+    public class clEmpleados
+    {
+        //Constructor se ejecuta automatico
+        public clEmpleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.0M;
+            Edad = 0;
+        }
+
+        //ATRIBUTOS
+        public string Nombre;
+
+        public decimal SueldoDiario;
+
+        public int Edad;
+
+        //METODOS
+
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return SueldoDiario * NumeroDias;
+        }
+
+    }
+}
